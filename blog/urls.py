338e7b7a7afdf461cd536baed22bb9360c3f49e2
Maulanawesome5 <maulanawesome5@gmail.com>
2022-07-21 12:55:17 +0700

    Delete existing Django apps

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'), # ini views yang ada didalam folder blog
-    path('cerita/', views.cerita, name='cerita'),
-    path('news/', views.news, name='news')
-]
