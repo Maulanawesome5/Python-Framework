d47588a03205bbd95cae193cf98d8c955c4e34d5
Maulanawesome5 <maulanawesome5@gmail.com>
2022-08-03 21:10:05 +0700

    App mywebsite dan blog sudah terhubung

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'judul' : 'Blog',
+        'subjudul' : 'Ini adalah Blog Kelas Terbuka',
+    }
+    return render(request, 'blog/index.html', context)
+    # pass
