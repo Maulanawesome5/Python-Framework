9d2ff747208f43ef1485bff0ffad450e53200c4a
Maulanawesome5 <maulanawesome5@gmail.com>
2022-08-03 17:35:57 +0700

    Reset project

--- mywebsite/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    context = {
-        'judul':'Kelas Terbuka',
-        'subjudul':'Selamat Datang',
-    }
-    return render(request, template_name="index.html", context=context)
-
